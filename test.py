c315712552c430ca3dfb3c8245e6d8a8e814f844
ChristopherRush <chris.uclan@gmail.com>
2018-06-11 20:59:57 +0100

    sadas

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def menu(title, choices):
 def item_chosen(button, choice):
     response = urwid.Text([u'You chose ', choice, u'\n'])
     done = urwid.Button(u'Ok')
-    urwid.connect_signal(done, 'click', menu)
+    urwid.connect_signal(button, 'click', menu, choices)
     main.original_widget = urwid.Filler(urwid.Pile([response,
         urwid.AttrMap(done, None, focus_map='reversed')]))
 
